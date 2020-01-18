490047527600e93853b80534a2202e9e9c7db640
gmck2323 <30579200+gmck2323@users.noreply.github.com>
2020-01-18 11:21:00 -0700

    add client back

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center'}}>
+            <h1>
+                Emaily!
+            </h1>
+            Collect feedback form your useres
+        </div>
+    );
+};
+
+export default Landing;
